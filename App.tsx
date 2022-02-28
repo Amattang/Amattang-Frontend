4d7f2d0781fb1a0c70b88029bd2be25fc57bdc66
11t518s <11t518s20161759@gmail.com>
2022-02-28 16:55:55 +0900

    feat: splash screen 안드로이드 splash screen #9

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
 import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import RootNav from './navigation/RootNav';
 import 'react-native-gesture-handler';
+import SplashScreen from 'react-native-splash-screen';
 
 function App() {
+  useEffect(() => {
+    setTimeout(() => SplashScreen.hide(), 1000);
+  }, []);
+
   return (
     <>
       <NavigationContainer>
