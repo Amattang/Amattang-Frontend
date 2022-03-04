099c5b554ca71173cd999445cd21015e64cdb0d7
11t518s <11t518s20161759@gmail.com>
2022-03-05 00:54:24 +0900

    feat: 온보딩 화면 onBoarding type a #14

--- screens/landing/OnBoarding.tsx
+++ screens/landing/OnBoarding.tsx
@@ -1,39 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Image, Pressable, ScrollView, Text, View } from 'react-native';
 import styles from './styles';
 import { OnBoardingStackProps } from '../../types/navigationTypes';
 
 function OnBoarding({ navigation }: OnBoardingStackProps) {
   const response = {
-    status: 200,
-    message: '카테고리 질문 목록 조회',
     data: [
       {
-        questionId: 1,
+        questionId: 'question1',
+        question: '창틀이 깨끗한가요?',
+        mainCategory: '외부 시설',
+        subCategory: null,
+        rule: null,
+        description: '곰팡이가 없는지 확인 후 청소를 요청하세요',
+        emoji: ':)',
+        type: 'A',
+        answer: {
+          answerId: 'answerId1',
+          ans: [
+            { type: '예', val: false, redType: false },
+            { type: '아니오', val: false, redType: true },
+          ],
+        },
+      },
+      {
+        questionId: 'question2',
+        question: '갈라진 틈이 있나요?',
+        mainCategory: '외부 시설',
+        subCategory: null,
+        rule: null,
+        description: '벽의 틈새를 통해 벌레가 출몰한다',
+        emoji: ':)',
+        type: 'A',
+        answer: {
+          answerId: 'answerId2',
+          ans: [
+            { type: '예', val: false, redType: false },
+            { type: '아니오', val: false, redType: true },
+          ],
+        },
+      },
+      {
+        questionId: 'question3',
+        question: '변기의 물이 잘 내려가나요?',
+        mainCategory: '외부 시설',
+        subCategory: '세면대 물을 틀어 놓은 상태로 변기 물을 내려보세요요요요요요 ',
+        rule: null,
+        description: '오래된 건물의 고층이라면 특히 주의해주세요',
+        emoji: ':)',
+        type: 'A',
+        answer: {
+          answerId: 'answerId3',
+          ans: [
+            { type: '예', val: false, redType: false },
+            { type: '아니오', val: false, redType: true },
+          ],
+        },
+      },
+      {
+        questionId: 'question4',
         question: '이중 잠금 장치가 설치 되어 있나요?',
         mainCategory: '외부 시설',
-        subCategory: 'null',
-        rule: 'null',
+        subCategory: null,
+        rule: null,
         description: '설치를 요구할 수 있어요',
         emoji: ':)',
         type: 'A',
         answer: {
-          answerId: 1,
-          ans: null,
-          redType: false,
+          answerId: 'answerId4',
+          ans: [
+            { type: '예', val: false, redType: false },
+            { type: '아니오', val: false, redType: true },
+          ],
         },
       },
       {
-        questionId: 2,
+        questionId: 'question5',
         question: '주변 편의시설을 확인하세요',
         mainCategory: '외부 시설',
-        subCategory: 'null',
+        subCategory: null,
         rule: '',
         description: '주소 입력시 표시되는 지도를 활용하세요',
         emoji: ':)',
         type: 'D',
         answer: {
-          answerId: '2',
+          answerId: 'answer5',
           ans: [
             {
               type: '병원',
@@ -64,6 +115,7 @@ function OnBoarding({ navigation }: OnBoardingStackProps) {
       },
     ],
   };
+  const [responsedData, setResponsedData] = useState(response.data);
   const onMapHandler = () => {
     navigation.navigate('map');
   };
@@ -71,18 +123,21 @@ function OnBoarding({ navigation }: OnBoardingStackProps) {
     <>
       <View style={styles.onBoardingFullScreen}>
         <View style={styles.upperElement}>
-          <Text style={styles.mainText}>우리집을 체크하며 {'\n'}아맞땅을미리 경험해보세요</Text>
+          <Text style={styles.mainText}>우리집을 체크하며 {'\n'}아맞땅을 미리 경험해보세요</Text>
         </View>
         <ScrollView style={styles.checkListCards}>
           <View style={styles.whiteCard}>
             <Text style={styles.checkListMainTitle}>주소를 입력하세요</Text>
             <View style={styles.buttonsOfCheckList}>
-              <Pressable style={[styles.directInputOfAddress, styles.buttonOfCheckLIst]}>
+              <Pressable
+                onPress={() => console.log('여기에 누르면 이제 지도 검색 궈궈')}
+                style={[styles.directInputOfAddress, styles.buttonOfCheckList]}
+              >
                 <Text style={styles.directInputTextOfAddress}>직접 입력</Text>
               </Pressable>
               <Pressable
                 onPress={onMapHandler}
-                style={[styles.mapInputOfAddress, styles.buttonOfCheckLIst]}
+                style={[styles.mapInputOfAddress, styles.buttonOfCheckList]}
               >
                 <Image
                   style={styles.mapInputImageOfAddress}
@@ -92,9 +147,81 @@ function OnBoarding({ navigation }: OnBoardingStackProps) {
               </Pressable>
             </View>
           </View>
+          {responsedData.map((data) => (
+            <View style={styles.whiteCard} key={data.questionId}>
+              {/*<CheckListComponent />*/}
+              <Text style={styles.checkListMainTitle}>{data.question}</Text>
+              <View style={styles.subTitles}>
+                {data.subCategory && (
+                  <View style={styles.checkListSubTitle}>
+                    <Text style={styles.emoji}>📘 </Text>
+                    <Text style={styles.checkListGrayText}>{data.subCategory}</Text>
+                  </View>
+                )}
+                {data.description && (
+                  <View style={styles.checkListSubTitle}>
+                    <Text style={styles.emoji}>👀 </Text>
+                    <Text style={styles.checkListGrayText}>{data.description}</Text>
+                  </View>
+                )}
+              </View>
+              <View style={styles.buttonsOfCheckList}>
+                {data.type === 'A' &&
+                  data.answer.ans.map((answer) => (
+                    <>
+                      <Pressable
+                        onPress={() =>
+                          setResponsedData(
+                            responsedData.map((questionItem) =>
+                              questionItem.questionId === data.questionId
+                                ? {
+                                    ...questionItem,
+                                    answer: {
+                                      ans: [
+                                        ...questionItem.answer.ans.map((answerItem) =>
+                                          answerItem.type === answer.type
+                                            ? { ...answerItem, val: true }
+                                            : { ...answerItem, val: false }
+                                        ),
+                                      ],
+                                    },
+                                  }
+                                : { ...questionItem }
+                            )
+                          )
+                        }
+                        style={
+                          answer.val
+                            ? answer.redType
+                              ? [styles.typeABtnWrapper, styles.checkListFocusedOrange]
+                              : [styles.typeABtnWrapper, styles.checkListFocusedBlue]
+                            : [styles.typeABtnWrapper]
+                        }
+                      >
+                        <View>
+                          <Text
+                            style={
+                              answer.val ? styles.checkListWhiteText : styles.checkListGrayText
+                            }
+                          >
+                            {answer.type}
+                          </Text>
+                        </View>
+                      </Pressable>
+                    </>
+                  ))}
+                {data.type === 'B' && <Text>b</Text>}
+                {data.type === 'C' && <Text>c</Text>}
+                {data.type === 'D' && <Text>d</Text>}
+                {data.type === 'E' && <Text>e</Text>}
+              </View>
+            </View>
+          ))}
         </ScrollView>
       </View>
-      <View></View>
+      <Pressable onPress={() => navigation.navigate('login')} style={styles.rightArrowWrapper}>
+        <Image source={require('../../assets/images/common/rightArrow.png')} />
+      </Pressable>
     </>
   );
 }
--- screens/landing/styles.tsx
+++ screens/landing/styles.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet } from 'react-native';
-import { mainBlack, mainBlue, mainLightBlue } from '../../color';
+import { mainBlack, mainBlue, mainLightBlue, mainOrange } from '../../color';
 
 const styles = StyleSheet.create({
   landingPageFullScreen: { backgroundColor: 'white', flex: 1 },
@@ -31,11 +31,16 @@ const styles = StyleSheet.create({
   kakaoLoginBtn: { backgroundColor: '#FEE500', marginTop: 12 },
   kakaoLoginText: {},
 
-  checkListCards: { marginTop: 30 },
-  whiteCard: { marginHorizontal: 17, backgroundColor: 'white', padding: 30, borderRadius: 14 },
+  checkListCards: { marginVertical: 30, marginHorizontal: 17 },
+  whiteCard: {
+    backgroundColor: 'white',
+    padding: 30,
+    borderRadius: 14,
+    marginVertical: 12,
+  },
   checkListMainTitle: { fontSize: 20 },
   buttonsOfCheckList: { marginTop: 50, flexDirection: 'row' },
-  buttonOfCheckLIst: {
+  buttonOfCheckList: {
     backgroundColor: mainLightBlue,
     borderRadius: 5,
     justifyContent: 'center',
@@ -48,6 +53,36 @@ const styles = StyleSheet.create({
   directInputTextOfAddress: { color: 'gray' },
   mapInputOfAddress: { width: 89, marginLeft: 14, flexDirection: 'row' },
   mapInputImageOfAddress: { marginRight: 8 },
+
+  subTitles: { marginTop: 19 },
+  checkListSubTitle: { flexDirection: 'row' },
+  checkListGrayText: { color: '#7C7C7C', lineHeight: 24 },
+  checkListWhiteText: { color: 'white', lineHeight: 24 },
+  typeABtnWrapper: {
+    marginRight: 19,
+    borderRadius: 4,
+    width: 100,
+    height: 44,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: mainLightBlue,
+  },
+  checkListFocusedBlue: { backgroundColor: mainBlue },
+  checkListFocusedOrange: { backgroundColor: mainOrange },
+
+  rightArrowWrapper: {
+    width: 55,
+    height: 55,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: mainBlue,
+    borderRadius: 55,
+    position: 'absolute',
+    right: 20,
+    bottom: 50,
+  },
+
+  emoji: { lineHeight: 24, fontSize: 12 },
 });
 
 export default styles;
