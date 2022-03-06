adbd3b7f79b715f63bb268f5df847fab50f215f7
11t518s <11t518s20161759@gmail.com>
2022-03-07 00:23:42 +0900

    feat: web web 환경설정 완료, 후에 서버 api 나오면 라우팅 연결 #18

---
+++ index.web.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Text } from 'react-native';
+
+function App() {
+  return <Text>hahaha</Text>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
