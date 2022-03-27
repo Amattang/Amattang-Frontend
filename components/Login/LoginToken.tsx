7092cd29127c115d98b53453cfe23aad573ab2df
unknown <lsj8249@naver.com>
2022-03-27 15:54:01 +0900

    refactor: kakao saveLogin

--- components/Login/LoginToken.tsx
+++ components/Login/LoginToken.tsx
@@ -4,17 +4,16 @@ import { API_HOST } from '../../constant';
 
 export const axiosInstance = axios.create({ baseURL: API_HOST });
 
-export const requestRefresh: TokenRefreshRequest = async (
-  refreshToken: string
-): Promise<string> => {
-  const response = await axios.post(`/issue/re`, {
-    request: 'access / refresh',
-    token: `Bearer ${refreshToken}`,
-  });
-  console.log(response.data.data.accessToken);
-  return response.data.data.accessToken;
-};
+// export const requestRefresh: TokenRefreshRequest = async (
+//   refreshToken: string
+// ): Promise<string> => {
+//   const response = await axios.post(`/issue/re`, {
+//     request: 'access / refresh',
+//     token: `Bearer ${refreshToken}`,
+//   });
+//   return response.data.data.token;
+// };
 
-applyAuthTokenInterceptor(axiosInstance, { requestRefresh });
+// applyAuthTokenInterceptor(axiosInstance, { requestRefresh });
 
-// 리프레시 만료됐을 때 login api
+// // 리프레시 만료됐을 때 login api
