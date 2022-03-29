1c9183da6fff97b4bb799171a6db49cc597c7b1f
11t518s <69836010+11t518s@users.noreply.github.com>
2022-03-29 15:19:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import App from './App';
 import { name as appName } from './app.json';
 
 // default
-axios.defaults.baseURL = 'http://3.35.229.111:8080';
+axios.defaults.baseURL = 'http://3.38.93.184:8080';
 axios.defaults.withCredentials = true;
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
